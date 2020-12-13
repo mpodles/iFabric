435521a9fec5e958e308267f92b85c7cc23a4bdb
mpodles <mp@kpodles.pl>
2020-12-13 17:01:49 +0100

    renamed mycontroller to incomplete, copied good controller from solution into p4runtime

--- src/p4runtime/mycontroller_incomplete.py
+++ src/p4runtime/mycontroller_incomplete.py
@@ -3,11 +3,6 @@ import argparse
 import os
 from time import sleep
 
-# NOTE: Appending to the PYTHON_PATH is only required in the `solution` directory.
-#       It is not required for mycontroller.py in the top-level directory.
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
 import p4runtime_lib.bmv2
 import p4runtime_lib.helper
 
@@ -62,17 +57,9 @@ def writeTunnelRules(p4info_helper, ingress_sw, egress_sw, tunnel_id,
     # and you will need to select the port dynamically for each switch based on
     # your topology.
 
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.myTunnel_exact",
-        match_fields={
-            "hdr.myTunnel.dst_id": tunnel_id
-        },
-        action_name="MyIngress.myTunnel_forward",
-        action_params={
-            "port": SWITCH_TO_SWITCH_PORT
-        })
-    ingress_sw.WriteTableEntry(table_entry)
-    print "Installed transit tunnel rule on %s" % ingress_sw.name
+    # TODO build the transit rule
+    # TODO install the transit rule on the ingress switch
+    print "TODO Install transit tunnel rule"
 
     # 3) Tunnel Egress Rule
     # For our simple topology, the host will always be located on the
@@ -105,18 +92,8 @@ def readTableRules(p4info_helper, sw):
             entry = entity.table_entry
             # TODO For extra credit, you can use the p4info_helper to translate
             #      the IDs the entry to names
-            table_name = p4info_helper.get_tables_name(entry.table_id)
-            print '%s: ' % table_name,
-            for m in entry.match:
-                print p4info_helper.get_match_field_name(table_name, m.field_id),
-                print '%r' % (p4info_helper.get_match_field_value(m),),
-            action = entry.action.action
-            action_name = p4info_helper.get_actions_name(action.action_id)
-            print '->', action_name,
-            for p in action.params:
-                print p4info_helper.get_action_param_name(action_name, p.param_id),
-                print '%r' % p.value,
-            print
+            print entry
+            print '-----'
 
 def printCounter(p4info_helper, sw, counter_name, index):
     '''
@@ -144,8 +121,8 @@ def main(p4info_file_path, bmv2_file_path):
 
     # Create a switch connection object for s1 and s2;
     # this is backed by a P4 Runtime gRPC connection
-    s1 = p4runtime_lib.bmv2.Bmv2SwitchConnection('s1', 
-                                                 address='127.0.0.1:50051',
+    s1 = p4runtime_lib.bmv2.Bmv2SwitchConnection('s1',
+                                                 address='127.0.0.1:50051', 
                                                  device_id=0)
     s2 = p4runtime_lib.bmv2.Bmv2SwitchConnection('s2', 
                                                  address='127.0.0.1:50052',
@@ -168,8 +145,8 @@ def main(p4info_file_path, bmv2_file_path):
                      dst_eth_addr="00:00:00:00:01:01", dst_ip_addr="10.0.1.1")
 
     # TODO Uncomment the following two lines to read table entries from s1 and s2
-    readTableRules(p4info_helper, s1)
-    readTableRules(p4info_helper, s2)
+    #readTableRules(p4info_helper, s1)
+    #readTableRules(p4info_helper, s2)
 
     # Print the tunnel counters every 2 seconds
     try:
